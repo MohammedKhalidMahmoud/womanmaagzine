6417cb01b39869bf8689cad0943815d979657d3d
mkhaled <mohammedkhalidmahmoud@gmail.com>
2025-04-26 13:44:31 +0300

    working on mapping

--- articles/articlescript.js
+++ articles/articlescript.js
@@ -4,9 +4,15 @@ function hello(){
   .then(res => res.json())
   .then(data => {
     console.log(data[1]);
-    document.querySelector("body").innerHTML=data[20].content;
+    document.querySelector("body").innerHTML=data[articleId-1].content;
   });
 }
 
 
 hello();
+const articleId = getQueryParam('id');
+console.log(articleId);
+function getQueryParam(param) {
+    const urlParams = new URLSearchParams(window.location.search);
+    return urlParams.get(param);
+}
--- sections/sectionscript.js
+++ sections/sectionscript.js
@@ -19,19 +19,19 @@ function display(articles){
     cartona=articles.map((el,index)=>{
         return `<div class="w-full md:w-1/2 lg:w-1/3 px-2" >
             <div class="mt-4 bg-white border border-gray-200 rounded-lg dark:bg-gray-800 dark:border-gray-700 ">
-                <a href="../articles/article.html" class="">
-                    <img class="w-full rounded-t-lg h-70  mb-1 block mx-auto" src="../images/section${sectionId}/article${index}/1.jpeg" alt="" />
+                <a href="../articles/article.html?id=${el.id}" class="">
+                    <img class="w-full rounded-t-lg  mb-1 block mx-auto h-[500px]" src="../images/section${sectionId}/article${index+1}/1.jpeg" alt="" />
                 </a>
                 <div class="p-5">
                     <div class="tags">
-                        <div class="bg-[#f25287] w-fit rounded-md px-2 py-0.5 text-white text-xs font-semibold ml-auto">${sectionName}</div>
+                        <div class="bg-[#f25287] w-fit rounded-md px-2 py-1 mb-1 text-white text-xs font-semibold ml-auto">${sectionName}</div>
                     </div>
-                    <a href="../articles/article.html">
+                    <a href="../articles/article.html?id=${el.id}">
                         <h5 class="text-right mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">${el.title}</h5>
                     </a>
                     
                     <p class="text-right mb-3 font-normal text-gray-700 dark:text-gray-400">${el.description}</p>
-                    <a href="../articles/article.html" class="block w-fit ml-auto  px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                    <a href="../articles/article.html?id=${el.id}" class="mt-1 block w-fit ml-auto  px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                         <i class="fa-solid fa-arrow-left text-white mr-1"></i>
                         اقرأ المزيد
                         <!-- <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
