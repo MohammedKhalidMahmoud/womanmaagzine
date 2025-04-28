8bfd4fda552d8aba2f846efb77435846cdf42780
mkhaled <mohammedkhalidmahmoud@gmail.com>
2025-04-28 21:38:01 +0300

    finsih main page

--- script.js
+++ script.js
@@ -1,40 +1,53 @@
 const carousel_div=document.querySelector("#carousel");
-const next_btn=document.querySelector("#next_button");
-const prev_btn=document.querySelector("#previous_button");
-console.log(next_btn);
-console.log(prev_btn);
+
+
 // console.log(carousel_div);
-const container=document.querySelector("#content_here")
+const a5bar_container=document.querySelector("#a5bar_here")
+const mkalat_container=document.querySelector("#mkalat_here")
+const tkarer_container=document.querySelector("#tkareer_here")
+const t7kekat_container=document.querySelector("#t7kekat_here")
+const hwarat_container=document.querySelector("#hwarat_here")
+console.log(a5bar_container, mkalat_container,tkarer_container,t7kekat_container,hwarat_container);
 function display(data){
     let cartona=''
-    const shuffled = data.sort(() => 0.5 - Math.random());
-    cartona=shuffled.map((el,index)=>{
-        return `<div class="w-full md:w-1/2 lg:w-1/3 px-2" >
-            <div class="mt-4 bg-white border border-gray-200 rounded-lg dark:bg-gray-800 dark:border-gray-700 ">
-                <a href="../articles/article.html?id=${el.id}" class="">
-                    <img class="w-full rounded-t-lg  mb-1 block mx-auto h-[500px]" src="../tabs/tab${el.section_id}/${index+1}.jpeg" alt="" />
-                </a>
-                <div class="p-5">
-                    <div class="tags">
-                        <div class="bg-[#f25287] w-fit rounded-md px-2 py-1 mb-1 text-white text-xs font-semibold ml-auto">${el.section_name}</div>
-                    </div>
-                    <a href="../articles/article.html?id=${el.id}">
-                        <h5 class="text-right mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">${el.title}</h5>
+    // const shuffled = data.sort(() => 0.5 - Math.random());
+    for(let j = 0; j < 5; j++) {
+        cartona='';
+        articles=data.filter(el=> j+1===+el.section_id);
+        console.log(articles)
+        for(let i=0;i<3;i++){
+            let el=articles[i];
+            cartona+=`<div class="w-full md:w-1/2 lg:w-1/3 px-2" >
+                <div class="mt-4 bg-white border border-gray-200 rounded-lg dark:bg-gray-800 dark:border-gray-700 ">
+                    <a href="../articles/article.html?id=${el.id}" class="">
+                        <img class="w-full rounded-t-lg  mb-1 block mx-auto h-[500px]" src="../tabs/tab${j+1}/${i+1}.jpeg" alt="" />
                     </a>
-                    
-                    <p class="text-right mb-3 font-normal text-gray-700 dark:text-gray-400">${el.description}</p>
-                    <a href="../articles/article.html?id=${el.id}" class="mt-1 block w-fit ml-auto  px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                        <i class="fa-solid fa-arrow-left text-white mr-1"></i>
-                        اقرأ المزيد
-                        <!-- <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-                        </svg> -->
-                    </a> 
-                </div>
-            </div>  
-        </div>  `
-    }).join('');
-    container.innerHTML=cartona;
+                    <div class="p-5">
+                        <div class="tags">
+                            <div class="bg-[#f25287] w-fit rounded-md px-2 py-1 mb-1 text-white text-xs font-semibold ml-auto">${el.section_name}</div>
+                        </div>
+                        <a href="../articles/article.html?id=${el.id}">
+                            <h5 class="text-right mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">${el.title}</h5>
+                        </a>
+                        
+                        <p class="text-right mb-3 font-normal text-gray-700 dark:text-gray-400">${el.description}</p>
+                        <a href="../articles/article.html?id=${el.id}" class="mt-1 block w-fit ml-auto  px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                            <i class="fa-solid fa-arrow-left text-white mr-1"></i>
+                            اقرأ المزيد
+                            <!-- <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                            </svg> -->
+                        </a> 
+                    </div>
+                </div>  
+            </div>  `
+        }
+            
+        // }
+        j===0? a5bar_container.innerHTML=cartona : j===1? mkalat_container.innerHTML=cartona : j===2? tkarer_container.innerHTML=cartona : j===3? t7kekat_container.innerHTML=cartona : hwarat_container.innerHTML=cartona;
+             
+    }
+    
 }
 
 const ul=document.querySelector("#ul_id")
